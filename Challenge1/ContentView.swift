aec14a1cd478e2eb8cf094ed671b51485f9233e3
Roba gh <rfalghanmi@gmail.com>
2022-10-27 13:17:34 +0300

    Initial Commit

---
+++ Challenge1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Challenge1
+//
+//  Created by roba on 27/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
